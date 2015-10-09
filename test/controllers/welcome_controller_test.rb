975fcd36cb60dc2e7fb66a304a361c4f40896977
franssjr <jrfranssell@gmail.com>
2015-10-09 17:56:47 -0400

    no gemlock

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,33 +2,33 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   # rubocop:disable Metrics/MethodLength
-#   def setup
-#     Message.create(the_message: 'It is certain')
-#     Message.create(the_message: 'It is decidedly so')
-#     Message.create(the_message: 'Without a doubt')
-#     Message.create(the_message: 'Yes definitely')
-#     Message.create(the_message: 'You may rely on it')
-#     Message.create(the_message: 'As I see it yes')
-#     Message.create(the_message: 'Most likely')
-#     Message.create(the_message: 'Outlook good')
-#     Message.create(the_message: 'Yes')
-#     Message.create(the_message: 'Signs point to yes')
-#     Message.create(the_message: 'Reply hazy try again')
-#     Message.create(the_message: 'Ask again later')
-#     Message.create(the_message: 'Better not tell you now')
-#     Message.create(the_message: 'Cannot predict now')
-#     Message.create(the_message: 'Concentrate and ask again')
-#     Message.create(the_message: "Don't count on it")
-#     Message.create(the_message: 'My reply is no')
-#     Message.create(the_message: 'My sources say no')
-#     Message.create(the_message: 'Outlook not so good')
-#     Message.create(the_message: 'Very doubtful')
-#   end
+  # def setup
+  #   Message.create(the_message: 'It is certain')
+  #   Message.create(the_message: 'It is decidedly so')
+  #   Message.create(the_message: 'Without a doubt')
+  #   Message.create(the_message: 'Yes definitely')
+  #   Message.create(the_message: 'You may rely on it')
+  #   Message.create(the_message: 'As I see it yes')
+  #   Message.create(the_message: 'Most likely')
+  #   Message.create(the_message: 'Outlook good')
+  #   Message.create(the_message: 'Yes')
+  #   Message.create(the_message: 'Signs point to yes')
+  #   Message.create(the_message: 'Reply hazy try again')
+  #   Message.create(the_message: 'Ask again later')
+  #   Message.create(the_message: 'Better not tell you now')
+  #   Message.create(the_message: 'Cannot predict now')
+  #   Message.create(the_message: 'Concentrate and ask again')
+  #   Message.create(the_message: "Don't count on it")
+  #   Message.create(the_message: 'My reply is no')
+  #   Message.create(the_message: 'My sources say no')
+  #   Message.create(the_message: 'Outlook not so good')
+  #   Message.create(the_message: 'Very doubtful')
+  # end
 
-#   test 'histories_created' do
-#     get :index
+  # test 'histories_created' do
+  #   get :index
 
-#     assert Message.all.any? { |m| response.body.include?(m.the_message) }
-#     assert response.body.include?(History.all.sort_by(&:created_at).first.message.the_message)
-#   end
+  #   assert Message.all.any? { |m| response.body.include?(m.the_message) }
+  #   assert response.body.include?(History.all.sort_by(&:created_at).first.message.the_message)
+  # end
 end
