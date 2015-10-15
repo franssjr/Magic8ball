8deb7c09265be826fce6e08eb339f551fd619824
franssjr <jrfranssell@gmail.com>
2015-10-14 21:35:07 -0400

    New readme

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -30,10 +30,9 @@ class MessagesControllerTest < ActionController::TestCase
   end
 
   test 'histories_created' do
-    # get :index
+    get :index
 
-    # assert Message.all.any? { |m| response.body.include?(m.the_message) }
-    # assert response.body.include?(History.all.sort_by(&:created_at).first.message.the_message)
-    assert true
+    assert Message.all.any? { |m| response.body.include?(m.the_message) }
+    assert response.body.include?(History.all.sort_by(&:created_at).first.message.the_message)
   end
 end
